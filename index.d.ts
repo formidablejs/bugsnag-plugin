58bcd57392459ae49e4732d1fdf4f082895c6367
Donald Pakkies <donaldpakkies@gmail.com>
2021-10-23 13:48:45 +0200

    feat(build): build imba and ts

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-import BugsnagServiceResolver from "./types/BugsnagServiceResolver";
-import { Event } from '@bugsnag/js';
-
-export interface UniversalBugsnagStatic {
-	registerCallback(callback: (event: Event) => void): void;
-}
-
-export { BugsnagServiceResolver };
--- types/BugsnagServiceResolver.d.ts
+++ types/BugsnagServiceResolver.d.ts
@@ -13,6 +13,7 @@ export default class BugsnagServiceResolver extends ServiceResolver {
         redactedKeys: any;
         releaseStage: any;
         sendCode: any;
+        logger: any;
     };
     catchQueries(): import("@formidablejs/framework").Application;
     /**
