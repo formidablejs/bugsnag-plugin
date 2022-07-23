a07a06746633608d9d6cf3de669b1c692d06a41f
Donald Pakkies <donaldpakkies@gmail.com>
2022-07-23 19:37:44 +0200

    feat: add typescript publishables

--- formidable/Package.js
+++ formidable/Package.js
@@ -1,9 +1,17 @@
 exports.Package = class Package {
-	publish() {
+	publish(language = 'imba') {
+		const ext = language.toLowerCase() == 'imba'
+		  ? 'imba' : (
+			language.toLowerCase() == 'typescript' ? 'ts' : 'imba'
+		  )
+
+		const configKey = `config/bugsnag.${ext}`;
+		const configValue = `./formidable/config/bugsnag.${ext}`;
+
 		return {
 			config: {
 				paths: {
-					'config/bugsnag.imba': './formidable/config/bugsnag.imba'
+					[configKey]: configValue
 				}
 			}
 		}
